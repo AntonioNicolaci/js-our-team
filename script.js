494fdf7b0b276c3f08df3a5d4605f89cf11bbb97
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-28 15:28:41 +0200

    add array of object

---
+++ script.js
@@ -0,0 +1,33 @@
+const team = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Graphic Designer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        nome:  "Barbara Ramos",
+        ruolo: "Chief Editor",
+        image: "barbara-ramos-graphic-designer.jpg"
+    },
+]
+
